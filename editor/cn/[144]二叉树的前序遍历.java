b0cfefc4adc0ccf5d7fabe20c9f110a2767de7be
Rancho <ranchocooper@gmail.com>
2020-06-09 11:47:23 +0800

    up

---
+++ editor/cn/[144]二叉树的前序遍历.java
@@ -0,0 +1,52 @@
+//给定一个二叉树，返回它的 前序 遍历。 
+//
+// 示例: 
+//
+// 输入: [1,null,2,3]  
+//   1
+//    \
+//     2
+//    /
+//   3 
+//
+//输出: [1,2,3]
+// 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+// Related Topics 栈 树
+
+
+package main
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.ArrayList;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private List<Integer> result = new ArrayList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) {
+            return result;
+        }
+
+        result.add(root.val);
+        if (root.left != null) {
+            preorderTraversal(root.left);
+        }
+        if (root.right != null) {
+            preorderTraversal(root.right);
+        }
+
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
