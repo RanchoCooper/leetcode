388809ad5b1283d5af8e7d66058332c2e70f4465
Rancho <ranchocooper@gmail.com>
2021-01-21 21:34:18 +0800

    01.tow sum

--- editor/cn/[1]两数之和.go
+++ editor/cn/[1]两数之和.go
@@ -1,31 +1,57 @@
-//Given an array of integers, return indices of the two numbers such that they a
-//dd up to a specific target. 
+//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。 
 //
-// You may assume that each input would have exactly one solution, and you may n
-//ot use the same element twice. 
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。 
 //
-// Example: 
+// 你可以按任意顺序返回答案。 
 //
 // 
-//Given nums = [2, 7, 11, 15], target = 9,
 //
-//Because nums[0] + nums[1] = 2 + 7 = 9,
-//return [0, 1].
+// 示例 1： 
+//
 // 
-// Related Topics 数组 哈希表
+//输入：nums = [2,7,11,15], target = 9
+//输出：[0,1]
+//解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,4], target = 6
+//输出：[1,2]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [3,3], target = 6
+//输出：[0,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 2 <= nums.length <= 103 
+// -109 <= nums[i] <= 109 
+// -109 <= target <= 109 
+// 只会存在一个有效答案 
+// 
+// Related Topics 数组 哈希表 
+// 👍 10116 👎 0
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
-package main
 func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
 	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i] + nums[j] == target {
-				return []int{i, j}
-			}
+		another := target - nums[i]
+		if _, ok := m[another]; ok {
+			return []int{m[another], i}
 		}
+		m[nums[i]] = i
 	}
-
 	return nil
 }
 //leetcode submit region end(Prohibit modification and deletion)
