d512631fd57035a52ca3c4994ecf533de3f19669
Rancho <ranchocooper@gmail.com>
2021-03-09 21:42:56 +0800

    205.reverse list

---
+++ editor/cn/[206]反转链表.go
@@ -0,0 +1,34 @@
+package main
+//反转一个单链表。
+//
+// 示例: 
+//
+// 输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL 
+//
+// 进阶: 
+//你可以迭代或递归地反转链表。你能否用两种方法解决这道题？ 
+// Related Topics 链表 
+// 👍 1560 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	var behind *ListNode
+
+	for head != nil {
+		next := head.Next
+		head.Next = behind
+		behind = head
+		head = next
+	}
+	return behind
+}
+//leetcode submit region end(Prohibit modification and deletion)
