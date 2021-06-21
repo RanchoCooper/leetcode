8acd9792f6c215a4b85ad0deaea3afa9a9af8537
Rancho <ranchocooper@gmail.com>
2021-06-22 07:34:25 +0800

    560. sub array sum

---
+++ editor/cn/[560]和为K的子数组.go
@@ -0,0 +1,35 @@
+package main
+//给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。 
+//
+// 示例 1 : 
+//
+// 
+//输入:nums = [1,1,1], k = 2
+//输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
+// 
+//
+// 说明 : 
+//
+// 
+// 数组的长度为 [1, 20,000]。 
+// 数组中元素的范围是 [-1000, 1000] ，且整数 k 的范围是 [-1e7, 1e7]。 
+// 
+// Related Topics 数组 哈希表 
+// 👍 960 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func subarraySum(nums []int, k int) int {
+	var result int
+	for i := 0; i < len(nums); i++ {
+		sum := 0
+		for j := i; j < len(nums); j++ {
+			sum += nums[j]
+			if sum == k {
+				result ++
+			}
+		}
+	}
+	return result
+}
+//leetcode submit region end(Prohibit modification and deletion)
