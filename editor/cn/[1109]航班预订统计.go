7b04ab4ce5c5287edea1aa181c78d62427b7d0e4
Rancho <ranchocooper@gmail.com>
2021-06-24 08:34:55 +0800

    up

---
+++ editor/cn/[1109]航班预订统计.go
@@ -0,0 +1,71 @@
+package main
+//这里有 n 个航班，它们分别从 1 到 n 进行编号。 
+//
+// 有一份航班预订表 bookings ，表中第 i 条预订记录 bookings[i] = [firsti, lasti, seatsi] 意味着在从 fi
+//rsti 到 lasti （包含 firsti 和 lasti ）的 每个航班 上预订了 seatsi 个座位。 
+//
+// 请你返回一个长度为 n 的数组 answer，其中 answer[i] 是航班 i 上预订的座位总数。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：bookings = [[1,2,10],[2,3,20],[2,5,25]], n = 5
+//输出：[10,55,45,25,25]
+//解释：
+//航班编号        1   2   3   4   5
+//预订记录 1 ：   10  10
+//预订记录 2 ：       20  20
+//预订记录 3 ：       25  25  25  25
+//总座位数：      10  55  45  25  25
+//因此，answer = [10,55,45,25,25]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：bookings = [[1,2,10],[2,2,15]], n = 2
+//输出：[10,25]
+//解释：
+//航班编号        1   2
+//预订记录 1 ：   10  10
+//预订记录 2 ：       15
+//总座位数：      10  25
+//因此，answer = [10,25]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 2 * 104 
+// 1 <= bookings.length <= 2 * 104 
+// bookings[i].length == 3 
+// 1 <= firsti <= lasti <= n 
+// 1 <= seatsi <= 104 
+// 
+// Related Topics 数组 Prefix Sum 
+// 👍 149 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func corpFlightBookings(bookings [][]int, n int) []int {
+	delta := make([]int,  n + 2)
+	for _, booking := range bookings {
+		first, last, seats := booking[0], booking[1], booking[2]
+		delta[first] += seats
+		delta[last + 1] -= seats
+	}
+	sum := make([]int, n + 1)
+
+	for i := 1; i <= n; i++ {
+		sum[i] = sum[i - 1] + delta[i]
+	}
+	for i := 1; i <= n; i++ {
+		sum[i - 1] = sum[i]
+	}
+	return sum[:len(sum) - 1]
+}
+//leetcode submit region end(Prohibit modification and deletion)
