9abd91ab475bdc4b10118a28051d7262bf734eed
Rancho <ranchocooper@gmail.com>
2021-07-11 09:37:16 +0800

    up

--- editor/cn/[88]合并两个有序数组.go
+++ editor/cn/[88]合并两个有序数组.go
@@ -37,18 +37,15 @@ package main
 
 //leetcode submit region begin(Prohibit modification and deletion)
 func merge(nums1 []int, m int, nums2 []int, n int)  {
-	i := m - 1
-	j := n - 1
-	for k := m + n - 1; k >= 0 ; k--{
-		if j < 0 || ( i >= 0 && nums1[i] >= nums2[j]) {
+	i, j := m - 1, n -1
+	for k := m + n - 1; k >= 0; k-- {
+		if j < 0 || (i >= 0 && nums1[i] >= nums2[j]) {
 			nums1[k] = nums1[i]
 			i--
 		} else {
 			nums1[k] = nums2[j]
 			j--
-
 		}
-
 	}
 }
 //leetcode submit region end(Prohibit modification and deletion)
