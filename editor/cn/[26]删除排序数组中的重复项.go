f202b55c333de9d5544c6f7524c05481c4ced6bc
Rancho <ranchocooper@gmail.com>
2021-08-05 17:45:41 +0800

    up

--- editor/cn/[26]删除排序数组中的重复项.go
+++ editor/cn/[26]删除排序数组中的重复项.go
@@ -50,7 +50,7 @@ func removeDuplicates(nums []int) int {
 	n := 0
 
 	for i := 0; i < len(nums); i++ {
-		if i == 0 || nums[i] != nums[i - 1] {
+		if i == 0 || nums[i] != nums[i-1] {
 			nums[n] = nums[i]
 			n++
 		}
