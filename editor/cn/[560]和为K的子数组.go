b0695585835d9b487afe99a42131349b350dddc5
Rancho <ranchocooper@gmail.com>
2021-08-05 17:22:19 +0800

    up

--- editor/cn/[560]和为K的子数组.go
+++ editor/cn/[560]和为K的子数组.go
@@ -20,16 +20,16 @@ package main
 
 //leetcode submit region begin(Prohibit modification and deletion)
 func subarraySum(nums []int, k int) int {
-	sum := make([]int, len(nums) + 1)
+	sum := make([]int, len(nums)+1)
 
-	// 构建前缀和
 	for i := 0; i < len(nums); i++ {
-		sum[i + 1] = sum[i] + nums[i]
+		sum[i+1] = sum[i] + nums[i]
 	}
+
 	ans := 0
 	for i := 0; i < len(nums); i++ {
 		for j := i; j < len(nums); j++ {
-			if sum[j + 1] - sum[i] == k {
+			if sum[j+1] - sum[i] == k {
 				ans++
 			}
 		}
