2bb37c7a753b0380cda8cc0b1af29544a859f83e
Rancho <rancho.z@binance.com>
2021-12-11 22:55:09 +0800

    clear java

--- src/main/java/leetcode_en/all_002_add_two_numbers/Solution.java
+++
@@ -1,9 +0,0 @@
-package leetcode_en.all_002_add_two_numbers;
-
-/**
- * @author rancho
- * @date 2019-04-22
- */
-public class Solution {
-
-}
--- src/main/java/leetcode_en/all_006_zigzag_conversion/Solution.java
+++
@@ -1,9 +0,0 @@
-package leetcode_en.all_006_zigzag_conversion;
-
-/**
- * @author rancho
- * @date 2019-05-16
- */
-public class Solution {
-
-}
