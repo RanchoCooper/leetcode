02347ba7385d804f85a4fb80127c8b91bf15f9cd
Rancho <ranchocooper@gmail.com>
2024-02-21 17:21:09 +0800

    up

--- editor/cn/[206]反转链表.go
+++ editor/cn/[206]反转链表.go
@@ -1,15 +1,46 @@
-package main
-//反转一个单链表。
+//给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
 //
-// 示例: 
+// 
+// 
+// 
+// 
+// 
 //
-// 输入: 1->2->3->4->5->NULL
-//输出: 5->4->3->2->1->NULL 
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,3,4,5]
+//输出：[5,4,3,2,1]
+// 
 //
-// 进阶: 
-//你可以迭代或递归地反转链表。你能否用两种方法解决这道题？ 
-// Related Topics 链表 
-// 👍 1560 👎 0
+// 示例 2： 
+// 
+// 
+//输入：head = [1,2]
+//输出：[2,1]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目范围是 [0, 5000] 
+// -5000 <= Node.val <= 5000 
+// 
+//
+// 
+//
+// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？ 
+//
+// Related Topics 递归 链表 👍 3501 👎 0
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
@@ -25,11 +56,12 @@ func reverseList(head *ListNode) *ListNode {
 
 	for head != nil {
 		next := head.Next
+		// 改一条边
 		head.Next = last
+		// last, head向后移动
 		last = head
 		head = next
 	}
-
 	return last
 }
 //leetcode submit region end(Prohibit modification and deletion)
