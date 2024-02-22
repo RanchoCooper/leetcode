9d7499b44ad1020b120653f48f5ff42e75bdf8dd
Rancho <ranchocooper@gmail.com>
2024-02-22 10:47:06 +0800

    up

--- editor/cn/[20]有效的括号.go
+++ editor/cn/[20]有效的括号.go
@@ -1,4 +1,3 @@
-package main
 //给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。 
 //
 // 有效字符串需满足： 
@@ -6,6 +5,7 @@ package main
 // 
 // 左括号必须用相同类型的右括号闭合。 
 // 左括号必须以正确的顺序闭合。 
+// 每个右括号都有一个对应的相同类型的左括号。 
 // 
 //
 // 
@@ -31,29 +31,16 @@ package main
 //输出：false
 // 
 //
-// 示例 4： 
-//
-// 
-//输入：s = "([)]"
-//输出：false
-// 
-//
-// 示例 5： 
-//
-// 
-//输入：s = "{[]}"
-//输出：true 
-//
 // 
 //
 // 提示： 
 //
 // 
-// 1 <= s.length <= 104 
+// 1 <= s.length <= 10⁴ 
 // s 仅由括号 '()[]{}' 组成 
 // 
-// Related Topics 栈 字符串 
-// 👍 2460 👎 0
+//
+// Related Topics 栈 字符串 👍 4348 👎 0
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
@@ -63,9 +50,9 @@ func isValid(s string) bool {
 	}
 
 	m := map[string]string {
-		")": "(",
-		"]": "[",
-		"}": "{",
+			")": "(",
+			"]": "[",
+			"}": "{",
 	}
 
 	stack := make([]string, 0)
